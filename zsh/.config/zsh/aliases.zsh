b9d0dc9b247c7e00c984ae2e2b55cd8e62aafaff
Anton Kashin <anton.m.kashin@gmail.com>
2022-02-04 01:55:31 +0300

    Zsh aliases adjusted.

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,7 +1,8 @@
 alias grep='grep --color'
 if command -v exa >/dev/null 2>&1; then
     alias ls="exa"
-    alias ll="exa -l -s name --group-directories-first"
+    alias ll="exa --long --sort=name --group-directories-first"
+    alias lm="exa --long --modified --sort=modified --group-directories-first"
     alias tree="exa -T"
 else
     alias ls="ls -h"
