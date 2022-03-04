075ea5fc2fc4b34dc692b8910517a67d00636a67
Anton Kashin <anton.m.kashin@gmail.com>
2022-03-05 01:50:08 +0300

    Zsh: BROWSER

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -2,6 +2,7 @@ export PATH=~/.local/bin:$PATH
 export LC_ALL=en_US.UTF-8
 export EDITOR=nvim
 export VISUAL=nvim
+export BROWSER=qutebrowser
 
 command -v bat >/dev/null 2>&1 \
     && export MANPAGER="sh -c 'col -bx | bat -l man -p'"
