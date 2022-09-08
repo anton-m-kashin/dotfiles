b9c7774e8aae24d84b01bdcda03ac7a733de3109
Anton Kashin <anton.m.kashin@gmail.com>
2022-09-09 01:07:37 +0300

    Nvim: go settings.

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -1,1 +1,9 @@
-require('go').setup()
+require('go').setup({
+    goimport = 'gopls',
+    gofmt = 'gopls',
+    max_line_len = 80,
+    lsp_inlay_hints = {
+        parameter_hints_prefix = "# ",
+        other_hints_prefix = "=> ",
+    }
+})
